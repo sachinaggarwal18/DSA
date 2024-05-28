d17499ddff325e19ce84aa48c8d0638f89c654a2
Sachin <raisethebat2020@gmail.com>
2024-05-28 22:41:31 +0530

    added lc 377

---
+++ leetcode/combinationSum4.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <cstring> // for memset
+
+using namespace std;
+
+class Solution
+{
+public:
+    int t[201][1001];
+    int n;
+
+    int solve(int ind, vector<int> &nums, int target)
+    {
+        if (target == 0)
+        {
+            return 1;
+        }
+
+        if (ind >= n || target < 0)
+        {
+            return 0;
+        }
+
+        if (t[ind][target] != -1)
+        {
+            return t[ind][target];
+        }
+
+        int take = solve(0, nums, target - nums[ind]);
+        int notTake = solve(ind + 1, nums, target);
+
+        return t[ind][target] = take + notTake;
+    }
+
+    int combinationSum4(vector<int> &nums, int target)
+    {
+        n = nums.size();
+        memset(t, -1, sizeof(t));
+        return solve(0, nums, target);
+    }
+};
+
+int main()
+{
+    Solution solution;
+    int n, target;
+
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    cout << "Enter the target sum: ";
+    cin >> target;
+
+    int result = solution.combinationSum4(nums, target);
+
+    cout << "Number of combinations that sum up to " << target << ": " << result << endl;
+
+    return 0;
+}
