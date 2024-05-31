302a722d2588f144e945fd9606fb55a1f9094c8b
Sachin <raisethebat2020@gmail.com>
2024-05-31 23:51:53 +0530

    memoization

--- leetcode/DP/longestCommonSubs.cpp
+++ leetcode/DP/longestCommonSubs.cpp
@@ -6,26 +6,34 @@ using namespace std;
 class Solution
 {
 public:
-    int solve(string &s1, string &s2, int i, int j)
+    int solve(string &s1, string &s2, int i, int j, vector<vector<int>> &dp)
     {
         if (i >= s1.length() || j >= s2.length())
         {
             return 0;
         }
 
+        if (dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+
         if (s1[i] == s2[j])
         {
-            return 1 + solve(s1, s2, i + 1, j + 1);
+            return dp[i][j] = 1 + solve(s1, s2, i + 1, j + 1, dp);
         }
         else
         {
-            return max(solve(s1, s2, i + 1, j), solve(s1, s2, i, j + 1));
+            return dp[i][j] = max(solve(s1, s2, i + 1, j, dp), solve(s1, s2, i, j + 1, dp));
         }
     }
 
     int longestCommonSubsequence(string text1, string text2)
     {
-        return solve(text1, text2, 0, 0);
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return solve(text1, text2, 0, 0, dp);
     }
 };
 
